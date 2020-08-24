9f09fb726cb34f8e99fa8bb3519b9cab170f6564
shinya-takano <dev.shinya.takano@gmail.com>
2020-08-25 01:09:57 +0900

    ボタンの挙動を実装

--- app/src/main/java/com/example/google/map/sample/MapsActivity.kt
+++ app/src/main/java/com/example/google/map/sample/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.example.google.map.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.LinearLayout
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -9,7 +10,6 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -18,12 +18,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
 
+    companion object {
+        // const val is only primitives or string
+        val MY_HOUSE_LAT_LNG = LatLng(35.699778, 139.576081)
+        val MY_OFFICE_LAT_LNG = LatLng(35.658208, 139.701636)
+        val KAMIKOUCHI_LAT_LNG = LatLng(36.251606, 137.653653)
+        val SHIRETOKO_LAT_LNG = LatLng(44.199790, 145.239638)
+        val KAGOSHIMA_LAT_LNG = LatLng(31.586445, 130.649869)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
 
         // CoordinatorLayout の 直下に配置している View を渡す
         bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottomSheetLayout))
+
+        // 仮でMyHouse=吉祥寺, MyOffice=渋谷
+        findViewById<Button>(R.id.buttonMyHouse).setOnClickListener {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MY_HOUSE_LAT_LNG, 16f))
+        }
+        findViewById<Button>(R.id.buttonMyOffice).setOnClickListener {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MY_OFFICE_LAT_LNG, 16f))
+        }
+        findViewById<Button>(R.id.buttonKamikouchi).setOnClickListener {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KAMIKOUCHI_LAT_LNG, 16f))
+        }
+        findViewById<Button>(R.id.buttonShiretoko).setOnClickListener {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SHIRETOKO_LAT_LNG, 16f))
+        }
+        findViewById<Button>(R.id.buttonKagoshima).setOnClickListener {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(KAGOSHIMA_LAT_LNG, 16f))
+        }
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
@@ -43,11 +69,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // TODO 初期値を東京にする
-        // TODO 初期値を現在地にする 別
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        // 初期値は自宅
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MY_HOUSE_LAT_LNG, 16f))
     }
 }
